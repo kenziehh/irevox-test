e6da242fcf35c4fb64eb39882e9fff5f55756729
kenziehh <ghaisani.nurani@gmail.com>
2025-08-09 02:39:34 +0700

    feat(cors): enable CORS with specific origin and headers for API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+
+  });
+
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      console.log('OPTIONS preflight request received:', req.headers);
+    }
+    next();
+  });
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
